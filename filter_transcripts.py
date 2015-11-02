ee27d56fe4a8ea0fc8983f2b3a668e165257dc0f
Sreeram K <ksreeram@D-10-65-19-15.dhcp4.washington.edu>
2015-11-02 14:35:38 -0800

    added transcript filtering

--- filter_transcripts.py
+++ filter_transcripts.py
@@ -1,41 +1,43 @@
 import util
 
-def write_cutoff(cutoff, weights, transcripts, false_positives):
-    weights = weights[int(0.1*cutoff*len(weights)):]
-    good_names = set([x[0] for x in weights])
 
-    transcripts = [(name, seq) for name, seq in transcripts if name in good_names]
-    hits = 0
-    for name, seq in transcripts:
-        if name in false_positives:
-            hits += 1
-    print('{} false positives for {}'.format(hits, cutoff))
-    util.seqs_to_fasta('drop{}/transcripts.fa'.format(cutoff), transcripts)
+def do_all(reconstr_file,fp_file,kal_file):
+    def write_cutoff(cutoff, weights, transcripts, false_positives):
+        weights = weights[int(0.1*cutoff*len(weights)):]
+        good_names = set([x[0] for x in weights])
 
-transcripts = util.from_fasta(reconstr_file)
+        transcripts = [(name, seq) for name, seq in transcripts if name in good_names]
+        hits = 0
+        for name, seq in transcripts:
+            if name in false_positives:
+                hits += 1
+        print('{} false positives for {}'.format(hits, cutoff))
+        util.seqs_to_fasta('drop{}/transcripts.fa'.format(cutoff), transcripts)
 
-weights = []
-zero = 0
-with open(kal_file) as f:
-    f.readline()
-    for line in f:
-        name, _, _, _, weight = line.split()
-        if float(weight) == 0:
-            zero += 1
-        weights.append((name, float(weight)))
-weights.sort(key = lambda x: x[1])
+    transcripts = util.from_fasta(reconstr_file)
 
-false_positives = set()
-with open(fp_file) as f:
-    for line in f:
-        name, code, length = line.split()
-        if int(length) < 200: continue
-        if code == '0':
-            false_positives.add(name)
-print('{} false positives'.format(len(false_positives)))
+    weights = []
+    zero = 0
+    with open(kal_file) as f:
+        f.readline()
+        for line in f:
+            name, _, _, _, weight = line.split()
+            if float(weight) == 0:
+                zero += 1
+            weights.append((name, float(weight)))
+    weights.sort(key = lambda x: x[1])
 
-for cutoff in range(10):
-    write_cutoff(cutoff, weights, transcripts, false_positives)
+    false_positives = set()
+    with open(fp_file) as f:
+        for line in f:
+            name, code, length = line.split()
+            if int(length) < 200: continue
+            if code == '0':
+                false_positives.add(name)
+    print('{} false positives'.format(len(false_positives)))
+
+    for cutoff in range(10):
+        write_cutoff(cutoff, weights, transcripts, false_positives)
 
 
 def main():
@@ -44,6 +46,7 @@ def main():
     else:
         arguments = sys.argv
     reconstr_file,fp_file,kal_file = arguments[1], arguments[2],arguments[3] 
+    do_all(reconstr_file,fp_file,kal_file)
 
 if __name__ == '__main__':
     main()
